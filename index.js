fdf7fa1e47d461e28373a67e947a5a988f8b9712
Anand Raju <araju8844@gmail.com>
2022-08-15 23:32:59 -0400

    basic pseudocode and all and found setinterval

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const getResponse = async () => {
     const data  = await axios(url);
     console.log(data["data"][data["data"].length - 1])
 }
-
-getResponse();
+// what we will do from here is add an if case to compare player ids
+// if they are the same (no new pick) you stay the same, otherwise you print a new Youtube URLy
+setInterval(getResponse, 2000);
 // axios.get(url).then(resp => {
 //     console.log(resp.data);
 // });
